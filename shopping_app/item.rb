fa639bda72349caca3cb915a5ad39ed0153832be
alexis <alexis@MacBook-Pro.local>
2022-03-21 15:42:14 +0200

    adding ownable method

---
+++ shopping_app/item.rb
@@ -0,0 +1,26 @@
+require_relative "ownable"
+
+class Item
+  include Ownable
+  attr_reader :name, :price
+
+  @@instances = []
+
+  def initialize(name, price, owner=nil)
+    @name = name
+    @price = price
+    self.owner = owner
+
+    # When an Item instance is created, the Item instance (self) is stored in a class variable named @@insntances.
+    @@instances << self
+  end
+
+  def label
+    { name: name, price: price }
+  end
+
+  def self.all
+# Returns @@instances ==> This means that Item.all returns all Item instances created so far.    @@instances
+  end
+
+end
